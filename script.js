8722aa342382fd327b61302bd187b95e3ce163f1
swethapasupuleti-git <141996206+swethapasupuleti-git@users.noreply.github.com>
2023-11-10 20:58:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function submitForm() {
+      var firstName = document.getElementById('firstname').value;
+      var lastName = document.getElementById('lastname').value;
+      var phoneNumber = document.getElementById('phNumber').value;
+      var email = document.getElementById('email').value;
+
+      var result = "First Name: " + firstName + "\nLast Name: " + lastName + "\nPh Number: " + phoneNumber + "\nEmail ID: " + email;
+
+      alert(result);
+}
